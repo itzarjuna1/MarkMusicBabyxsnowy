220d32f9276944a54e33ef099c30b6e864d8d8c7
ARJUN <pachangedinanath@gmail.com>
2025-07-17 17:01:15 +0530

    Update __init__.py

--- MARKX/__init__.py
+++ MARKX/__init__.py
@@ -3,7 +3,16 @@ from MARKX.core.dir import dirr
 from MARKX.core.git import git
 from MARKX.core.userbot import Userbot
 from MARKX.misc import dbb, heroku, sudo
+import asyncio
 from aiohttp import ClientSession
+
+aiohttpsession = None
+
+async def init_session():
+    global aiohttpsession
+    aiohttpsession = ClientSession()
+
+asyncio.run(init_session()) 
 
 from .logging import LOGGER
 
