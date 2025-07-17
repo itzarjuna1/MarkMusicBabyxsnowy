8f0afd24f795eb36d2d45b569086385dc28909ab
ARJUN <pachangedinanath@gmail.com>
2025-07-17 17:28:23 +0530

    Update __init__.py

--- MARKX/__init__.py
+++ MARKX/__init__.py
@@ -5,14 +5,14 @@ from MARKX.core.bot import MARKXBot
 from MARKX.core.dir import dirr
 from MARKX.core.git import git
 from MARKX.core.userbot import Userbot
-from MARKX.misc import db, heroku, sudo
+from MARKX.misc import dbb, heroku, sudo
 from .logging import LOGGER
 
-# Declare globals at module level
+# Globals for bot clients
 app = None
 userbot = None
 
-# Declare API wrappers as None initially
+# Globals for APIs
 YouTube = None
 Carbon = None
 Spotify = None
@@ -21,6 +21,7 @@ Resso = None
 SoundCloud = None
 Telegram = None
 
+# Global aiohttp session
 aiohttpsession = None
 
 async def init_session():
@@ -36,17 +37,21 @@ async def startup():
     global app, userbot
     global YouTube, Carbon, Spotify, Apple, Resso, SoundCloud, Telegram
 
+    # Init HTTP session
     await init_session()
 
+    # Setup tasks
     dirr()
     git()
-    db()
+    dbb()
     heroku()
     sudo()
 
+    # Initialize bot clients
     app = MARKXBot()
     userbot = Userbot()
 
+    # Import and initialize API wrappers
     from .platforms import YouTubeAPI, CarbonAPI, SpotifyAPI, AppleAPI, RessoAPI, SoundAPI, TeleAPI
 
     YouTube = YouTubeAPI()
