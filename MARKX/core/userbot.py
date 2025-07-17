be0ffa52f61608a3e48c47630cd8629c63f7a5d5
ARJUN <pachangedinanath@gmail.com>
2025-07-17 19:04:36 +0530

    Update userbot.py

--- MARKX/core/userbot.py
+++ MARKX/core/userbot.py
@@ -11,36 +11,36 @@ assistantids = []
 class Userbot:
     def __init__(self):
         self.one = Client(
-            session_name=StringSession(config.STRING1),
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
+            StringSession(config.STRING1),
+            config.API_ID,
+            config.API_HASH,
             no_updates=True,
         ) if config.STRING1 else None
 
         self.two = Client(
-            session_name=StringSession(config.STRING2),
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
+            StringSession(config.STRING2),
+            config.API_ID,
+            config.API_HASH,
             no_updates=True,
         ) if config.STRING2 else None
 
         self.three = Client(
-            session_name=StringSession(config.STRING3),
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
+            StringSession(config.STRING3),
+            config.API_ID,
+            config.API_HASH,
             no_updates=True,
         ) if config.STRING3 else None
 
         self.four = Client(
-            session_name=StringSession(config.STRING4),
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
+            StringSession(config.STRING4),
+            config.API_ID,
+            config.API_HASH,
             no_updates=True,
         ) if config.STRING4 else None
 
         self.five = Client(
-            session_name=StringSession(config.STRING5),
-            api_id=config.API_ID,
-            api_hash=config.API_HASH,
+            StringSession(config.STRING5),
+            config.API_ID,
+            config.API_HASH,
             no_updates=True,
         ) if config.STRING5 else None
